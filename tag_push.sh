c8039b5009149c26d6e37dcbdc4f864a9dbed220
lincyaw <814750204@qq.com>
2025-05-01 10:34:05 +0800

    update chart build scripts

--- tag_push.sh
+++ tag_push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 原始镜像列表
 images=(
   "ghcr.io/chaos-mesh/chaos-mesh-e2e:latest"
   "ghcr.io/chaos-mesh/e2e-helper:latest"
@@ -9,15 +8,11 @@ images=(
   "ghcr.io/chaos-mesh/chaos-daemon:latest"
 )
 
-# 目标仓库前缀
 target_registry="10.10.10.240/library"
 
-# 循环 tag 镜像
 for image in "${images[@]}"; do
-  # 提取镜像名（去掉 ghcr.io/chaos-mesh/）
   image_name=$(echo "$image" | awk -F'/' '{print $NF}')
   
-  # 构建新镜像名
   new_image="${target_registry}/${image_name}"
 
   echo "Tagging $image as $new_image"
